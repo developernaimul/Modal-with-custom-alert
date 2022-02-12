aa9c93db9abd87d973dcd4a2b0423bf51b6df717
developernaimul <developernaimul@gmail.com>
2022-02-12 14:49:34 +0600

    Alert function add

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const button = document.getElementById('apply');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 const banner = document.querySelector('.banner');
+const discard = document.querySelector('.discard');
+const keepedit = document.querySelector('.edit');
+const alert = document.querySelector('.alert');
+const editclose = document.querySelector('.edit-close');
+
 
 button.addEventListener('click', function(){
 
@@ -11,14 +16,38 @@ button.addEventListener('click', function(){
     banner.style.backdropFilter = 'blur(30px)';
 });
 
-close.addEventListener('click', function(){
-    modal.classList.remove('active');
-    banner.style.backdropFilter = 'blur(10px)';
-});
+// close.addEventListener('click', function(){
+//     modal.classList.remove('active');
+//     banner.style.backdropFilter = 'blur(10px)';
+// });
 
 banner.addEventListener('click', function(e){
     if( e.target == this ){
         modal.classList.remove('active');
         banner.style.backdropFilter = 'blur(10px)';
+    }
+});
+
+// Alert
+
+close.addEventListener('click', function(){
+    alert.style.display = 'flex';
+});
+editclose.addEventListener('click', function(){
+    alert.style.display = 'none';
+});
+keepedit.addEventListener('click', function(){
+    alert.style.display = 'none';
+});
+discard.addEventListener('click', function(){
+    alert.style.display = 'none';
+    modal.classList.remove('active');
+    banner.style.backdropFilter = 'blur(10px)';
+});
+
+
+alert.addEventListener('click', function(e){
+    if(e.target == this){
+        this.style.display = 'none';
     }
 });
